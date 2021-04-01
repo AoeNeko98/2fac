a5cdf40c1c41304134e77c3e427d6a223d105d9e
Samer Achour <samer.achour@esprit.tn>
2021-04-01 03:36:54 +0100

    final

---
+++ src/Main/Main.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Main.view.BookEditDialogController;
+import entity.Book;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import Main.view.BookController;
+
+import static javafx.application.Application.launch;
+import javafx.scene.Parent;
+import javafx.stage.Modality;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+    static Connection cnx;
+    static Statement st;
+    static ResultSet rst;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("../piamine/Login.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Light");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        //this.primaryStage.setTitle("AddressApp");
+        
+        //  initRootLayout();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+
+    }
+}
---
+++ src/service/UserService.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+import Main.MyDB;
+
+import entity.User;
+import iservice.IUser;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aoe Neko 98
+ */
+public class UserService implements IUser{
+     
+    Connection conn ;
+     
+
+    public UserService() {
+         this.conn = MyDB.getInstance().getConnexion();
+
+    
+}
+
+    @Override
+    public ArrayList<User> getAllUsers() {
+        
+        ArrayList<User> ls = new ArrayList<>();
+        ResultSet rs ;
+        User user ;
+        String sql ="SELECT * FROM user ;";
+        try {
+            Statement stl = conn.createStatement();
+            rs = stl.executeQuery(sql);
+            while(rs.next()){
+                user = new User();
+                
+                 user.setNom(rs.getString("nom"));
+                
+                 user.setId(rs.getInt("id_User"));
+                 ls.add(user);
+            }
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        return ls ;
+       
+    }
+}
